5316f4fba658a80cf176b78a8d795cee302945ef
aehamdy <ae.hamdy91@gmail.com>
2024-01-29 15:54:13 +0200

    Added keyboard accessibility

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputElements = document.querySelectorAll(".card__input");
 const submitButton = document.querySelector(".card__button");
 
+// a function to calculate and return the age
 const calculateAge = (year, month, day) => {
     const today = new Date();
     const birthdate = new Date(year, month - 1, day);
@@ -14,6 +15,7 @@ const calculateAge = (year, month, day) => {
     return age;
 }
 
+// a function that gets user inputs and pass to calculateAge function to show the result to the user
 const onClickHandler = () => {
     const dayElement = document.querySelector(".card__input[name='day']");
     const monthElement = document.querySelector(".card__input[name='month']");
@@ -22,5 +24,10 @@ const onClickHandler = () => {
 
     resultElement.textContent = calculateAge(yearElement.value, monthElement.value, dayElement.value);
 }
+
+// Add keyboard accessibility
+inputElements.forEach((input) => {
+    input.addEventListener("keydown", event => event.key === "Enter" && onClickHandler());
+})
 
 submitButton.addEventListener("click", onClickHandler);
