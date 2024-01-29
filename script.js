46fdeff00f2f9c1f851d241fb81914bb51bdd736
aehamdy <ae.hamdy91@gmail.com>
2024-01-29 18:23:35 +0200

    Added validation

--- script.js
+++ script.js
@@ -1,6 +1,52 @@
 const inputElements = document.querySelectorAll(".card__input");
 const submitButton = document.querySelector(".card__button");
 
+const validateDay = (day) => {
+    if (day && day > 0 && day <= 31) {
+        return true;
+    }
+}
+
+const validateMonth = (month) => {
+    if (month && month > 0 && month <= 12) {
+        return true;
+    }
+}
+
+const validateYear = (year) => {
+    const currentYear = new Date().getFullYear();
+    if (year && year > 0 && year <= currentYear) {
+        return true;
+    }
+}
+
+const isDateValid = (dayElement, monthElement, yearElement) =>  {
+    let isValid = [false, false, false];
+
+    if (!validateDay(dayElement.value)) {
+        dayElement.classList.add("card__input--error");
+    } else {
+        isValid[0] = true;
+        dayElement.classList.remove("card__input--error");
+    }
+
+    if (!validateMonth(monthElement.value)) {
+        monthElement.classList.add("card__input--error");
+    } else {
+        isValid[1] = true;
+        monthElement.classList.remove("card__input--error");
+    }
+
+    if (!validateYear(yearElement.value)) {
+        yearElement.classList.add("card__input--error");
+    } else {
+        isValid[2] = true;
+        yearElement.classList.remove("card__input--error");
+    }
+
+    return isValid.every((item) => item === true);
+}
+
 // a function to calculate and return the age
 const calculateAge = (year, month, day) => {
     const today = new Date();
@@ -21,6 +67,11 @@ const onClickHandler = () => {
     const monthElement = document.querySelector(".card__input[name='month']");
     const yearElement = document.querySelector(".card__input[name='year']");
     const resultElement = document.querySelector(".card__resultValue");
+
+    if (!isDateValid(dayElement, monthElement, yearElement)) {
+        resultElement.textContent = "--";
+        return;
+    }
 
     resultElement.textContent = calculateAge(yearElement.value, monthElement.value, dayElement.value);
 }
